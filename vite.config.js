7cdd892adaaf8ac4b49dd3c3105e8d0c2c8a2881
0xsmiler <anthonylee110.eng@gmail.com>
2025-03-16 23:23:46 +0000

    configured the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,23 @@ import eslint from "vite-plugin-eslint";
 export default defineConfig(() => {
   return {
     build: {
-      outDir: "build",
+      outDir: "dist",
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor'; // Dependencies in a separate chunk
+            }
+            if (id.includes('Peedy')) {
+              return 'peedy-chunk'; // Custom chunk for Peedy-related code
+            }
+            if (id.includes('Rocky')) {
+              return 'rocky-chunk'; // Custom chunk for Rocky-related code
+            }
+          },
+        },
+      },
       commonjsOptions: {
         esmExternals: true,
       },
